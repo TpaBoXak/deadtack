481e59c803f7d08e70a693f23f1c638b0aad7227
Арсений <gl.arseniy@mail.ru>
2024-04-07 23:23:17 +0300

    Изменил порт mariadb

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     DB_CONNECTOR = "mariadb+mariadbconnector"
     DB_USER = "root"
     DB_PASSWORD = "12345"
-    DB_HOST = "127.0.0.1:3307"
+    DB_HOST = "127.0.0.1:3306"
     DB_NAME = "deadtrack"
     SQLALCHEMY_DATABASE_URI = f"{DB_CONNECTOR}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8"
     LANGUAGES = ['ru']
